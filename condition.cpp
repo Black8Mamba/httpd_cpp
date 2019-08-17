20f66528913a8543dc5f48253a71ecddd9d60135
jiyongjie <jyj_student@163.com>
2019-08-17 19:48:07 +0800

    add condition

---
+++ condition.cpp
@@ -0,0 +1,17 @@
+#include "condition.h"
+#include <errno.h>
+
+bool Condition::waitForSeconds(double seconds)
+{
+  struct timespec abstime;
+  // FIXME: use CLOCK_MONOTONIC or CLOCK_MONOTONIC_RAW to prevent time rewind.
+  clock_gettime(CLOCK_REALTIME, &abstime);
+  // abstime.tv_sec += seconds;
+  const int64_t kNanoSecondsPerSecond = 1e9;
+  int64_t nanoseconds = static_cast<int64_t>(seconds * kNanoSecondsPerSecond);
+
+  abstime.tv_sec += static_cast<time_t>((abstime.tv_nsec + nanoseconds) / kNanoSecondsPerSecond);
+  abstime.tv_nsec = static_cast<long>((abstime.tv_nsec + nanoseconds) % kNanoSecondsPerSecond);
+  MutexLock::UnassignGuard ug(mutex_);
+  return ETIMEDOUT == pthread_cond_timedwait(&pcond_, mutex_.getPthreadMutex(), &abstime);    
+}
