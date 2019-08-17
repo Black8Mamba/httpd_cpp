084af252ad5e15026ce60d991296eec743aa5aab
jiyongjie <jyj_student@163.com>
2019-08-17 15:34:43 +0800

    完成优先级队列

--- priority_queue.cpp
+++ priority_queue.cpp
@@ -8,3 +8,57 @@
 
 #include "priority_queue.h"
 
+void yj_priority_queue::add_timer(http_request* request, 
+                                  size_t timeout, 
+                                function<int(http_request*)> func)
+{
+    //申请timer节点
+    yj_timer* timer_node = new yj_timer();
+    //request->timer = timer_node;
+    request->timer_ = timer_node;
+    //更新当前时间
+    yj_timer::time_update();
+    // 加入时设置超时阈值，删除信息,handler等
+    timer_node->set_key(yj_timer::current_msec + timeout);
+    timer_node->set_deleted(false);
+    timer_node->set_handler(func);
+    timer_node->set_request(request);
+
+    //新节点插入队列
+    yj_priority_queue::insert(timer_node);
+}
+
+void yj_priority_queue::del_timer(http_request* request)
+{
+    //更新当前时间
+    yj_timer::time_update();   
+    yj_timer* timer_node = request->timer_;
+    timer_node->set_deleted(true);
+}
+
+void yj_priority_queue::handle_expire_timers(void)
+{
+    while(!is_empty()) {    
+        yj_timer* timer_node = min(); //获取最小节点
+        if (timer_node->is_deleted()) { //如果被标记为删除
+            delmin(); //pop
+            delete timer_node; //释放空间
+            continue;
+        }
+        //更新当前时间
+        yj_timer::time_update(); 
+        if (timer_node->get_key() > yj_timer::current_msec) {
+            return; //未超时
+        }
+
+        //超时未被删除 调用handler处理
+        if (timer_node->get_func()) {
+            function<int(http_request*)> func = timer_node->get_func();//考虑指针优化
+            func(timer_node->get_request());
+        }
+
+        delmin();
+        delete timer_node;
+    }
+}
+
--- priority_queue.h
+++ priority_queue.h
@@ -11,9 +11,11 @@
 #include <queue>
 #include <vector>
 #include "timer.h"
+#include <functional>
 
 using std::priority_queue;
 using std::vector;
+using std::function;
 
 struct timer_comp //重写仿函数
 {
@@ -27,11 +29,21 @@ class yj_priority_queue
 {
 public:
     yj_priority_queue() { ; }
+
+    void add_timer(http_request* request, size_t timeout, 
+                    function<int(http_request*)> func); //添加定时器
+    void del_timer(http_request* request);  //删除定时器
+    void handle_expire_timers(void);  //超时处理
+
+
+private:
     bool is_empty(void) { return yj_pq_.empty(); }
     size_t size(void) { return yj_pq_.size(); }
     yj_timer* min(void) { return yj_pq_.top(); }
     void delmin(void) {  yj_pq_.pop(); }
     void insert(yj_timer* timer) { yj_pq_.push(timer); }
+
+
 
 private:
     priority_queue<yj_timer*, vector<yj_timer*>, timer_comp>  yj_pq_;
